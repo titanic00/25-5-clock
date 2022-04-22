bc74955bce3ff2abe3cfdcd06e578f168e9a371b
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-04-22 23:04:39 +0300

    added stopwatch and events "play", "stop" and "refresh"

--- recourses/script/main.js
+++ recourses/script/main.js
@@ -1,32 +1,111 @@
 $(document).ready(function() {
-    // set initial value for break length and session length
-    let breakLength_initialValue = 5;
-    let sessionLength_initialValue = 25;
+    // initial values
+    const breakLength = 5;
+    const sessionLength = 25;
+    // set values for break length and session length by clicking buttons on the page
+    let breakLength_dynamycValue = 5;
+    let sessionLength_dynamycValue = 25;
+    let seconds = 59;
+    let isPauseClicked = false;
+    let isRefreshClicked = false;
     // view values on the web page
-    $('#break-length-value').html(`${breakLength_initialValue}`);
-    $('#session-length-value').html(`${sessionLength_initialValue}`);
+    $('#break-length-value').html(`${breakLength_dynamycValue}`);
+    $('#session-length-value').html(`${sessionLength_dynamycValue}`);
+    $('#time').html(`${sessionLength_dynamycValue}:00`);
 
     // decrease and inrease value of break length
     $('.break-length-decrease').on('click', () => {
-        if (breakLength_initialValue == 1)
+        if (breakLength_dynamycValue == 1)
             return;
-        breakLength_initialValue -= 1;
-        $('#break-length-value').html(`${breakLength_initialValue}`);
+        breakLength_dynamycValue -= 1;
+        $('#break-length-value').html(`${breakLength_dynamycValue}`);
     });
     $('.break-length-increase').on('click', () => {
-        breakLength_initialValue += 1;
-        $('#break-length-value').html(`${breakLength_initialValue}`);
+        breakLength_dynamycValue += 1;
+        $('#break-length-value').html(`${breakLength_dynamycValue}`);
     });
 
     // decrease and inrease value of session length
     $('.session-length-decrease').on('click', () => {
-        if (sessionLength_initialValue == 1)
+        if (sessionLength_dynamycValue == 1)
             return;
-        sessionLength_initialValue -= 1;
-        $('#session-length-value').html(`${sessionLength_initialValue}`);
+        sessionLength_dynamycValue -= 1;
+        $('#session-length-value').html(`${sessionLength_dynamycValue}`);
+        $('#time').html(`${sessionLength_dynamycValue}:00`);
     });
     $('.session-length-increase').on('click', () => {
-        sessionLength_initialValue += 1;
-        $('#session-length-value').html(`${sessionLength_initialValue}`);
+        sessionLength_dynamycValue += 1;
+        $('#session-length-value').html(`${sessionLength_dynamycValue}`);
+        $('#time').html(`${sessionLength_dynamycValue}:00`);
+    });
+
+    const handleClick = (isPaused, isRefreshed) => {
+        if (isPaused) {
+            isPauseClicked = true;
+            return;
+        } else if (isRefreshed) {
+            isRefreshClicked = true;
+            return;
+        }
+
+        //
+        if (seconds === 59) {
+            sessionLength_dynamycValue -= 1;
+            $('#time').html(`${sessionLength_dynamycValue}:${seconds}`);
+            seconds -= 1;
+        } else if (seconds === 0) {
+            seconds = 59;
+            $('#time').html(`${sessionLength_dynamycValue}:${seconds}`);
+        } else if (seconds < 10) {
+            $('#time').html(`${sessionLength_dynamycValue}:0${seconds}`);
+            seconds -= 1;
+        } else {
+            $('#time').html(`${sessionLength_dynamycValue}:${seconds}`);
+            seconds -= 1;
+        }
+        return;
+    }
+
+    //
+    $('#start').on('click', () => {
+        const stopwatch = setInterval(() => {
+            /* if (!isPauseClicked) {
+                 handleClick(null, null);
+             } else if (isPauseClicked) {
+                 clearInterval(stopwatch);
+                 isPauseClicked = false;
+             } else if (isRefreshClicked) {
+                 $('#break-length-value').html(`${breakLength}`);
+                 $('#session-length-value').html(`${sessionLength}`);
+                 $('#time').html(`${sessionLength}:00`);
+                 breakLength_dynamycValue = breakLength;
+                 sessionLength_dynamycValue = sessionLength;
+                 clearInterval(stopwatch);
+                 isRefreshClicked = false;
+             } */
+
+            if (isPauseClicked) {
+                clearInterval(stopwatch);
+                isPauseClicked = false;
+            } else if (isRefreshClicked) {
+                clearInterval(stopwatch);
+                $('#break-length-value').html(`${breakLength}`);
+                $('#session-length-value').html(`${sessionLength}`);
+                $('#time').html(`${sessionLength}:00`);
+                breakLength_dynamycValue = breakLength;
+                sessionLength_dynamycValue = sessionLength;
+                isRefreshClicked = false;
+            } else {
+                handleClick(null, null);
+            }
+        }, 1000);
+    });
+
+    $('#pause').on('click', () => {
+        handleClick(true, null)
+    });
+
+    $('#reset').on('click', () => {
+        handleClick(null, true)
     });
 });
