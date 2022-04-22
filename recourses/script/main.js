ad23ad7a67582861e7643f9248170faa4968a854
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-04-22 19:40:06 +0300

    added logic for break-length and session-length elements

--- recourses/script/main.js
+++ recourses/script/main.js
@@ -1,8 +1,32 @@
 $(document).ready(function() {
-    let countDownDate = 2500;
-    var x = setInterval(function() {
+    // set initial value for break length and session length
+    let breakLength_initialValue = 5;
+    let sessionLength_initialValue = 25;
+    // view values on the web page
+    $('#break-length-value').html(`${breakLength_initialValue}`);
+    $('#session-length-value').html(`${sessionLength_initialValue}`);
 
-        countDownDate -= 1;
-        $('#time').html(countDownDate);
-    }, 1000);
+    // decrease and inrease value of break length
+    $('.break-length-decrease').on('click', () => {
+        if (breakLength_initialValue == 1)
+            return;
+        breakLength_initialValue -= 1;
+        $('#break-length-value').html(`${breakLength_initialValue}`);
+    });
+    $('.break-length-increase').on('click', () => {
+        breakLength_initialValue += 1;
+        $('#break-length-value').html(`${breakLength_initialValue}`);
+    });
+
+    // decrease and inrease value of session length
+    $('.session-length-decrease').on('click', () => {
+        if (sessionLength_initialValue == 1)
+            return;
+        sessionLength_initialValue -= 1;
+        $('#session-length-value').html(`${sessionLength_initialValue}`);
+    });
+    $('.session-length-increase').on('click', () => {
+        sessionLength_initialValue += 1;
+        $('#session-length-value').html(`${sessionLength_initialValue}`);
+    });
 });
